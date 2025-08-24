ef8e541b2f86f2cc34031e076b24a529401e9542
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-24 22:32:57 +0300

    add missing changes

--- src/app/chapter1/app.routes.server.ts
+++ src/app/chapter1/app.routes.server.ts
@@ -4,7 +4,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: Routes = [
   {
     path: 'sets',
-    loadComponent: () => import('./set/set').then(m => m.SetComponent),
+    loadComponent: () => import('./base-definitions/set/set').then(m => m.SetComponent),
+    title: 'Sets',
+  },
+  {
+    path: 'numbers',
+    loadComponent: () => import('./base-definitions/numbers/numbers').then(m => m.Numbers),
     title: 'Sets',
   }
 ];
